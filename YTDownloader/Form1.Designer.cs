335f1008b1d9df3f01616145b8282184bda41757
penealfa <penealfeleke17@gmail.com>
2022-06-22 21:41:29 +0300

    Add project files.

---
+++ YTDownloader/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace YTDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(35, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Url";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtURL.ForeColor = System.Drawing.Color.LightGray;
+            this.txtURL.Location = new System.Drawing.Point(91, 12);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(619, 36);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "Ex https://www.youtube.com/watch..";
+            this.txtURL.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtURL_MouseClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(304, 63);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(206, 32);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Download";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(36, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblStatus.Location = new System.Drawing.Point(99, 63);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(33, 20);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "......";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(726, 106);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ YTDownloader/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+using MediaToolkit;
+using System.IO;
+
+namespace YTDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean format = true;
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog dlg = new FolderBrowserDialog() { Description= "please select a folder" })
+            {
+                if(dlg.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show("Download has started please wait...", "Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    var yt = YouTube.Default;
+                    var Video = await yt.GetVideoAsync(txtURL.Text);
+                    lblStatus.Text = "Downloading...";
+                    btnStart.Enabled = false;
+                    try
+                    {
+                        File.WriteAllBytes(dlg.SelectedPath + @"\" + Video.FullName, await Video.GetBytesAsync());
+
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("No Ethernet Connection Please Try Again");
+                        throw;
+                    }
+                    
+                    var inputfile = new MediaToolkit.Model.MediaFile { Filename = dlg.SelectedPath + @"\" + Video.FullName };
+                    var outputfile = new MediaToolkit.Model.MediaFile { Filename = $"{dlg.SelectedPath + @"\" + Video.FullName}.mp3" };
+
+
+
+                    using (var enging =new Engine())
+                    {
+                        enging.GetMetadata(inputfile);
+                        enging.Convert(inputfile,outputfile);
+                    }
+                    if (format == true)
+                    {
+                        File.Delete(dlg.SelectedPath + @"\" + Video.FullName);
+                    }
+                    else
+                    {
+                        File.Delete($"{dlg.SelectedPath + @"\" + Video.FullName}.mp3");
+                    }
+                    
+                    MessageBox.Show("Download is Completed!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtURL.Text = "";
+                    lblStatus.Text = "......";
+                    btnStart.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Please Select a Folder :", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+            }
+
+        }
+
+        private void txtURL_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtURL.Clear();
+            txtURL.ForeColor = Color.Black;
+        }
+    }
+}
